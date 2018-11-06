d4dc280a157f9a3a895b4e13a853faab25bea51f
Mario Santos <mario@engsolutions.com.br>
2018-11-06 14:16:23 -0200

    initial commit

---
+++ src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa história</h2>
+        <div>Lorem impsum...</div>
+    </div>
+);
